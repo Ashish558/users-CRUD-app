843af44a349f2faebbcd28c8acc9492f1454e8e0
Ashish Prasad <77892788+Ashish558@users.noreply.github.com>
2022-03-11 22:34:44 +0530

    Update constants.js

--- client/src/services/constants.js
+++ client/src/services/constants.js
@@ -1,4 +1,4 @@
 
 // export const domain = 'https://desolate-mesa-23396.herokuapp.com'
-export const domain = 'http://localhost:4000'
+export const domain = 'https://user-crud-app-545.herokuapp.com'
 
